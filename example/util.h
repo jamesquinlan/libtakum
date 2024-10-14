1d02f9663960d13dc4b5bd67633c1f8f4232e35b
Laslo Hunhold <hunhold@uni-koeln.de>
2024-10-14 10:50:22 +0200

    Add multi-precision RPN calculator as an example
    Signed-off-by: Laslo Hunhold <hunhold@uni-koeln.de>

--- example/util.h
+++ example/util.h
@@ -2,6 +2,14 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <stdint.h>
+#include <stdio.h>
+
 #include "../takum.h"
 
-#endif
+#undef LEN
+#define LEN(x) (sizeof(x) / sizeof(*(x)))
+
+intmax_t getline(char **, size_t *, FILE *);
+
+#endif /* UTIL_H */
